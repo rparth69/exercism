ab3f6ab68fb1058074cc8e01bdb4bcc200d2bb91
rparth69 <rparth69@gmail.com>
2019-07-18 15:36:14 +0530

    exercism-4

---
+++ gigasecond/sample.rb
@@ -0,0 +1,42 @@
+# puts "enter the 1 to 7"
+# days =gets.chomp.to_i
+
+
+
+
+# puts"-------------For loop----------------"
+# for i in 1..days  do
+# 	puts i
+
+# end
+
+# puts"-------------case----------------"
+
+# 	case days
+
+# 		when "1"
+# 			puts"monday"
+# 		when "2"
+# 			puts"tueseday"
+# 		when "3"
+# 			puts"wenday"
+# 		when "4"
+# 			puts"thursday"
+# 		when "5"
+# 			puts"friday"
+# 		when "6"
+# 			puts"saturaday"
+# 		when "7"
+# 			puts"sunday"
+
+#         else 
+#         	puts"enter only 1 to 7"
+# 	end
+
+	loop do   
+  puts "Checking for answer"   
+  answer = gets.chomp   
+  if answer != '5'   
+    break   
+  end   
+end  
